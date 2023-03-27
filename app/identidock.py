ba354669af791dd6f6364dd655c91759dbf6358d
S C <alucal1355@ieselcaminas.org>
2023-03-27 18:51:09 +0200

    prueba web

--- app/identidock.py
+++ app/identidock.py
@@ -1,10 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
-
+default_name = 'Víctor Ponz'
 @app.route('/')
-def hello_world():
-    return 'Hello World from Ciberseguridad!\n'
-
+def get_identicon():
+	name = default_name
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+			Hola <input type="text" name="name" value="{}">
+			<input type="submit" value="submit">
+			</form>
+			<p>Te pareces a:
+			<img src="/monster/monster.png"/>
+			'''.format(name)
+	footer = '</body></html>'
+	return header + body + footer
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+	app.run(debug=True, host='0.0.0.0')
